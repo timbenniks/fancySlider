dc8112f34987dd6e8806b7468e25f371f1445b22
Tim Benniks <tbenniks@gmail.com>
2012-07-21 13:15:18 +0200

    Updated to use grunt.js

---
+++ test/fancySlider_test.js
@@ -0,0 +1,13 @@
+/*global QUnit:false, module:false, test:false, asyncTest:false, expect:false*/
+/*global start:false, stop:false ok:false, equal:false, notEqual:false, deepEqual:false*/
+/*global notDeepEqual:false, strictEqual:false, notStrictEqual:false, raises:false*/
+(function($)
+{
+	module('fancySlider');
+
+	test('fancy?', 1, function()
+	{
+		ok(true, 'fancySlider is very fancy indeed');
+	});
+
+}(jQuery));
